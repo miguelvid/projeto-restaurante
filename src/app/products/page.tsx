3be71993f81c91e75c4112bd2ecb607e06bd3e9b
Miguel Vidor de Olieveira <142550095+miguelvid@users.noreply.github.com>
2025-03-04 20:07:39 -0300

    feat: add shadcn and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const ProductPage = () => {
+    return (
+        <div>
+            <h1 className="text-red-400 p-5 text-center ">Product</h1>
+            <Button></Button>
+        </div>
+     );
+}
+ 
+export default ProductPage;
