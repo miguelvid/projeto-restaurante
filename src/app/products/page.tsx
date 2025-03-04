28ee0ff555cbfe76f7df63d2499ec4fc97ffc9b6
Miguel Vidor de Olieveira <142550095+miguelvid@users.noreply.github.com>
2025-03-04 20:29:15 -0300

    chore: add plugins /eslint_sort  /prettie_tailwind

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 const ProductPage = () => {
     return (
         <div>
-            <h1 className="text-red-400 p-5 text-center ">Product</h1>
+            <h1 className="text-red-400 p-5 rounded-xl">Product</h1>
             <Button></Button>
         </div>
      );
