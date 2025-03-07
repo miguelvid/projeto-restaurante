e1a6c082e7c6e5dc79574df0423dd15b0ed7f612
miguelvid <miguelvid.1909@gmail.com>
2025-03-07 17:32:32 -0300

    fix: fixed eslint plugins

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
 import { FlatCompat } from "@eslint/eslintrc";
-import simpleImportSort from "eslint-config-simple-import-sort";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,12 +1,12 @@
 import { Button } from "@/components/ui/button";
 
 const ProductPage = () => {
-    return (
-        <div>
-            <h1 className="text-red-400 p-5 rounded-xl">Product</h1>
-            <Button></Button>
-        </div>
-     );
-}
- 
+  return (
+    <div>
+      <h1 className="rounded-xl text-red-400">Product</h1>
+      <Button></Button>
+    </div>
+  );
+};
+
 export default ProductPage;
